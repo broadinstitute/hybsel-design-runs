2ae86704ba2279051a3d5c635d0552d3427188b4
Hayden Metsky <hmetsky@gmail.com>
2015-03-06 10:25:19 -0500

    Updated matrix entries

--- set-cover/make-matrix.sh
+++ set-cover/make-matrix.sh
@@ -3,11 +3,11 @@
 #for c in $(seq 0.1 0.1 1.0); do
 #  bsub -o make-matrix.out -q forest -R "rusage[mem=2]" "python ~/viral/hybsel_design/bin/make_probes_by_set_cover.py -d ebola_zaire -l 100 -m 0 -c $c >> matrix.vals"
 #done
-#for c in $(seq 0.1 0.1 1.0); do
-#  bsub -o make-matrix.out -q forest -R "rusage[mem=2]" "python ~/viral/hybsel_design/bin/make_probes_by_set_cover.py -d ebola_zaire -l 85 -m 2 -c $c >> matrix.vals"
-#done
-for l in $(seq 80 5 100); do
-  for m in $(seq 0 1 5); do
-    bsub -o make-matrix.out -q forest -R "rusage[mem=2]" "python ~/viral/hybsel_design/bin/make_probes_by_set_cover.py -d ebola_zaire -l $l -m $m -c 1.0 >> matrix.vals"
-  done
+for c in $(seq 0.1 0.1 1.0); do
+  bsub -o make-matrix.out -q forest -R "rusage[mem=2]" "python ~/viral/hybsel_design/bin/make_probes_by_set_cover.py -d ebola_zaire -l 100 -m 2 -c $c >> matrix.vals"
 done
+#for l in $(seq 80 5 100); do
+#  for m in $(seq 0 1 5); do
+#    bsub -o make-matrix.out -q forest -R "rusage[mem=2]" "python ~/viral/hybsel_design/bin/make_probes_by_set_cover.py -d ebola_zaire -l $l -m $m -c 1.0 >> matrix.vals"
+#  done
+#done
