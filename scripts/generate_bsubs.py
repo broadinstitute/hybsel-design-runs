54c44ee2420b920ce141ff2e68aff163bc5cb66f
Hayden Metsky <hmetsky@gmail.com>
2015-06-05 16:47:13 -0400

    Update generate_bsubs.py script with more datasets, parameter space, and memory requirements

--- scripts/generate_bsubs.py
+++ scripts/generate_bsubs.py
@@ -1,3 +1,5 @@
+import os
+
 DATASETS = [
             "chikungunya",
             "crimean_congo",
@@ -6,6 +8,8 @@ DATASETS = [
             "ebola_nonzaire",
             "hepatitis_a",
             "hepatitis_c",
+            "hiv1_without_ltr",
+            "hiv2_without_ltr",
             "influenza",
             "lassa",
             "marburg",
@@ -14,10 +18,32 @@ DATASETS = [
             "rhabdovirus",
             "rift_valley_fever",
             "sars",
-            "yellow_fever"
+            "yellow_fever",
            ]
 
-RESULTS_PATH = "~/viral/viral-work/results/hybsel_design/viral-probe-set_05-2015/"
+RESULTS_PATH = "/home/unix/hmetsky/viral/viral-work/results/hybsel_design/viral-probe-set_06-2015/"
+
+# parameter space is (mismatches, cover_extension)
+PARAMETER_SPACE = [(mismatches, cover_extension)
+                   for mismatches in range(0, 10)
+                   for cover_extension in range(0, 51, 10)]
+
+def mem_requested(dataset_name, mismatches, cover_extension):
+    big_datasets = ["hepatitis_c", "influenza", "hiv1_without_ltr"]
+    if dataset_name in big_datasets:
+        if mismatches >= 7:
+            return 24
+        elif mismatches >= 4:
+            return 16
+        else:
+            return 8
+    else:
+        if mismatches >= 7:
+            return 16
+        elif mismatches >= 4:
+            return 8
+        else:
+            return 4
 
 for dataset in DATASETS:
     if isinstance(dataset, tuple):
@@ -30,17 +56,28 @@ for dataset in DATASETS:
         # dataset
         name = dataset
         dataset = [dataset]
-    cmd = ["bsub"]
-    cmd += ["-o", RESULTS_PATH + name + ".out"]
-    cmd += ["-q", "forest"]
-    cmd += ["python", "bin/make_probes.py"]
-    cmd += ["-m", "3", "-l", "100"]
-    cmd += ["--dataset"] + dataset
-    cmd += ["--skip_adapters"]
-    cmd += ["--print_analysis"]
-    cmd += ["--write_analysis_to_tsv", RESULTS_PATH + name + ".analysis.tsv"]
-    cmd += ["--write_sliding_window_coverage", RESULTS_PATH + name + ".covg"]
-    cmd += ["-o", RESULTS_PATH + name + ".fasta"]
-    cmd += ["--verbose"]
-
-    print ' '.join(cmd)
+    # Make the directory for this dataset's results
+    if not os.path.exists(RESULTS_PATH + name):
+        os.makedirs(RESULTS_PATH + name)
+    for params in PARAMETER_SPACE:
+        mismatches, cover_extension = params
+        path = (RESULTS_PATH + name + "/mismatches_" + str(mismatches) +
+                "-coverextension_" + str(cover_extension))
+        mem = mem_requested(name, mismatches, cover_extension)
+        cmd = ["bsub"]
+        cmd += ["-o", path +  ".out"]
+        cmd += ["-q", "week"]
+        cmd += ["-R", "\"rusage[mem=" + str(mem) + "]\""]
+        cmd += ["-P", "hybseldesign"]
+        cmd += ["python", "bin/make_probes.py"]
+        cmd += ["--mismatches", str(mismatches)]
+        cmd += ["--lcf_thres", "100"]
+        cmd += ["--cover_extension", str(cover_extension)]
+        cmd += ["--dataset"] + dataset
+        cmd += ["--skip_adapters"]
+        cmd += ["--print_analysis"]
+        cmd += ["--write_analysis_to_tsv", path + ".analysis.tsv"]
+        cmd += ["--write_sliding_window_coverage", path + ".covg"]
+        cmd += ["-o", path + ".fasta"]
+        cmd += ["--verbose"]
+        print ' '.join(cmd)
