b7904720400950648f9ba1c3d05b9f7b08d88304
Hayden Metsky <hmetsky@gmail.com>
2015-06-30 15:25:13 -0400

    Allow custom adapter sequences when adding adapters to probes in a probe set

--- scripts/generate_bsubs_for_adapter_addition.py
+++ scripts/generate_bsubs_for_adapter_addition.py
@@ -2,29 +2,35 @@ import os
 
 DATASETS = [
             "chikungunya",
-            "crimean_congo",
-            "dengue",
+#            "crimean_congo",
+#            "dengue",
             (["ebola_zaire", "ebola2014"], "ebola_zaire-with-2014"),
-            "ebola_nonzaire",
-            "gbv_c",
-            "hepatitis_a",
-            "hepatitis_c",
-            "hiv1_without_ltr",
-            "hiv2_without_ltr",
-            "influenza",
+#            "ebola_nonzaire",
+#            "gbv_c",
+#            "hepatitis_a",
+#            "hepatitis_c",
+#            "hiv1_without_ltr",
+#            "hiv2_without_ltr",
+#            "influenza",
             "lassa",
-            "marburg",
-            "measles",
-            "mers",
-            "rhabdovirus",
-            "rift_valley_fever",
-            "sars",
-            "yellow_fever",
+#            "marburg",
+#            "measles",
+#            "mers",
+#            "rhabdovirus",
+#            "rift_valley_fever",
+#            "sars",
+#            "yellow_fever",
            ]
 
 ORIG_RESULTS_PATH = "/home/unix/hmetsky/viral/viral-work/results/hybsel_design/viral-probe-set_06-2015/"
-ADAPTER_RESULTS_PATH = "/home/unix/hmetsky/viral/viral-work/results/hybsel_design/viral-probe-set_06-2015_with-adapters/"
-PARAMS_PATH = "/home/unix/hmetsky/viral/viral-work/results/hybsel_design/viral-probe-set_06-2015_with-adapters/params.90000.txt"
+ADAPTER_RESULTS_PATH = "/home/unix/hmetsky/viral/viral-work/results/hybsel_design/viral-probe-set_06-2015_limited_with-adapters/"
+PARAMS_PATH = "/home/unix/hmetsky/viral/viral-work/results/hybsel_design/viral-probe-set_06-2015_limited_with-adapters/params.12000.txt"
+
+# To use custom adapter sequences, set ADAPTER_SEQUENCES_PATH to be a file where
+# each line is of the following format:
+# [dataset name]	[A adapter 5' end]..[A adapter 3' end]	[B adapter 5' end]..[B adapter 3' end]
+# (to use default adapter sequences, set ADAPTER_SEQUENCES_PATH to None)
+ADAPTER_SEQUENCES_PATH = "/home/unix/hmetsky/viral/viral-work/results/hybsel_design/viral-probe-set_06-2015_limited_with-adapters/adapters.txt"
 
 PARAMS = {}
 with open(PARAMS_PATH) as f:
@@ -33,6 +39,19 @@ with open(PARAMS_PATH) as f:
         dataset = ls[0]
         params = eval(ls[1])
         PARAMS[dataset] = params
+
+if ADAPTER_SEQUENCES_PATH != None:
+    # Use custom adapters
+    ADAPTER_SEQUENCES = {}
+    with open(ADAPTER_SEQUENCES_PATH) as f:
+        for line in f:
+            ls = line.rstrip().split('\t')
+            dataset = ls[0]
+            adapter_a = tuple(ls[1].split('..'))
+            adapter_b = tuple(ls[2].split('..'))
+            ADAPTER_SEQUENCES[dataset] = (adapter_a, adapter_b)
+else:
+    ADAPTER_SEQUENCES = None
 
 for dataset in DATASETS:
     if isinstance(dataset, tuple):
@@ -45,10 +64,12 @@ for dataset in DATASETS:
         # dataset
         name = dataset
         dataset = [dataset]
+
     mismatches, cover_extension = PARAMS[name]
     orig_fasta_path = (ORIG_RESULTS_PATH + name + "/mismatches_" + str(mismatches) +
                        "-coverextension_" + str(cover_extension) + ".fasta")
     path = ADAPTER_RESULTS_PATH + name
+
     mem = 4
     cmd = ["bsub"]
     cmd += ["-o", path +  ".out"]
@@ -62,6 +83,11 @@ for dataset in DATASETS:
     cmd += ["--dataset"] + dataset
     cmd += ["--filter_from_fasta", orig_fasta_path]
     cmd += ["--skip_set_cover"]
+    if ADAPTER_SEQUENCES != None:
+        # Use custom adapters
+        adapter_a, adapter_b = ADAPTER_SEQUENCES[name]
+        cmd += ["--adapter_a"] + list(adapter_a)
+        cmd += ["--adapter_b"] + list(adapter_b)
     cmd += ["--print_analysis"]
     cmd += ["-o", path + ".fasta"]
     cmd += ["--verbose"]
