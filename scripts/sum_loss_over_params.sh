8b42f8a7d2ba1c05377e0ab57968d5d2a5e06b1d
Hayden Metsky <hmetsky@gmail.com>
2016-02-04 11:47:45 -0500

    Fix typo

--- scripts/sum_loss_over_params.sh
+++ scripts/sum_loss_over_params.sh
@@ -6,6 +6,6 @@
 
 # The loss for a single dataset is defined by the 'mismatches' and
 # 'cover_extension' parameter values assigned to that dataset:
-#  LOSS = mismatches^2 A
+#  LOSS = mismatches^2 + (cover_extension / 10.0)^2
 
 awk -F'\t' '{print $2}' $1 | sed 's/(//' | sed 's/)//' | sed 's/, /\t/' | awk '{print $1*$1 + ($2/10.0)*($2/10.0)}' | awk '{sum+=$0} END {print sum}'
