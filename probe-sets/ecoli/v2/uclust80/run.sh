f8c7439f1d122fbcc801ea0d9f60751194b24f51
Hayden Metsky <hmetsky@gmail.com>
2017-09-14 11:30:15 -0400

    Add script to generate E coli probe set with '--island_of_exact_match'
    This probe set is in
      probe-sets/ecoli/v2/uclust80/probes/m2-e25-iem30.all-probes.fasta
    but is left out of this commit (it's large and not a final probe set).

--- probe-sets/ecoli/v2/uclust80/run.sh
+++ probe-sets/ecoli/v2/uclust80/run.sh
@@ -10,16 +10,20 @@ if [[ $1 == "make-commands" ]]; then
     # shortest sequence is 60.
     for m in 2; do
         for e in 25; do
-            mkdir -p probes/m${m}-e${e}
+            mkdir -p probes/m${m}-e${e}-iem30
             for x in input/*; do
                 f=$(echo "$x" | awk -F'/' '{print $2}')
-                cmd="make_probes.py -pl 75 -ps 25 -l 75 -m $m -e $e --small_seq_min 60 --skip_reverse_complements --skip_adapters -d custom:${x} -o probes/m${m}-e${e}/${f}.fasta --verbose > log/m${m}-e${e}.${f}.out 2> log/m${m}-e${e}.${f}.err"
+                cmd="make_probes.py -pl 75 -ps 25 -l 75 -m $m -e $e --island_of_exact_match 30 --small_seq_min 60 --skip_reverse_complements --skip_adapters -d custom:${x} -o probes/m${m}-e${e}-iem30/${f}.fasta --max_num_processes 2 --verbose > log/m${m}-e${e}-iem30.${f}.out 2> log/m${m}-e${e}-iem30.${f}.err"
                 echo "$cmd" >> $COMMANDS
             done
         done
     done
 elif [[ $1 == "parallel" ]]; then
+    source activate /ebs/hybsel-design-runs/tools/envs/hybseldesign-prod
+
     parallel --no-notice --progress < $COMMANDS
+
+    source deactivate
 else
     echo "FATAL: Unknown subcommand"
     exit 1
