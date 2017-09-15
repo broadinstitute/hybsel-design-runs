39cbacf763c4353199758f4fa1294b0ee8407f84
Hayden Metsky <hmetsky@gmail.com>
2017-09-15 14:40:49 -0400

    Add script to generate E coli probe set with '--expand_n'
    This probe set is in
      probe-sets/ecoli/v2/uclust80/probes/m2-e25-iem30-expandn.all-probes.fasta
    but is left out of this commit (it's large and not a final probe set).

--- probe-sets/ecoli/v2/uclust80/run.sh
+++ probe-sets/ecoli/v2/uclust80/run.sh
@@ -10,10 +10,10 @@ if [[ $1 == "make-commands" ]]; then
     # shortest sequence is 60.
     for m in 2; do
         for e in 25; do
-            mkdir -p probes/m${m}-e${e}-iem30
+            mkdir -p probes/m${m}-e${e}-iem30-expandn
             for x in input/*; do
                 f=$(echo "$x" | awk -F'/' '{print $2}')
-                cmd="make_probes.py -pl 75 -ps 25 -l 75 -m $m -e $e --island_of_exact_match 30 --small_seq_min 60 --skip_reverse_complements --skip_adapters -d custom:${x} -o probes/m${m}-e${e}-iem30/${f}.fasta --max_num_processes 2 --verbose > log/m${m}-e${e}-iem30.${f}.out 2> log/m${m}-e${e}-iem30.${f}.err"
+                cmd="make_probes.py -pl 75 -ps 25 -l 75 -m $m -e $e --island_of_exact_match 30 --expand_n --small_seq_min 60 --skip_reverse_complements --skip_adapters -d custom:${x} -o probes/m${m}-e${e}-iem30-expandn/${f}.fasta --max_num_processes 2 --verbose > log/m${m}-e${e}-iem30-expandn.${f}.out 2> log/m${m}-e${e}-iem30-expandn.${f}.err"
                 echo "$cmd" >> $COMMANDS
             done
         done
