5bcd3bbc7774c4f6b5295f5190c3ff50a8178558
Hayden Metsky <hmetsky@gmail.com>
2017-09-29 10:01:38 -0400

    Add script to generate E coli probe set with blacklisted sequences
    This probe set is in
      probe-sets/ecoli/v2-with-blacklist/uclust80/probes/m2-e25-iem30-expandn.all-probes.fasta
    but is left out of this commit (it's large and not a final probe set).

---
+++ probe-sets/ecoli/v2-with-blacklist/uclust80/run.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+COMMANDS="commands.txt"
+
+if [[ $1 == "make-commands" ]]; then
+    echo -n "" > $COMMANDS
+
+    # Generate a space-separated list of genomes to blacklist; put
+    # 'custom:' in front of each so that make_probes.py will read
+    # it as a custom fasta input
+    blacklisted_genomes=$(ls -1 blacklist-genomes-subset/*.fa | awk '{print "custom:"$0}' | tr '\n' ' ' | sed 's/ $//')
+
+    # Use '--small_seq_min 60' because a small number of sequences (1475)
+    # have a length that is less than the probe length, and the length of the
+    # shortest sequence is 60.
+    for m in 2; do
+        for e in 25; do
+            mkdir -p probes/m${m}-e${e}-iem30-expandn
+            for x in input/*; do
+                f=$(echo "$x" | awk -F'/' '{print $2}')
+                cmd="make_probes.py -pl 75 -ps 25 -l 75 -m $m -e $e --island_of_exact_match 30 -mt 8 --island_of_exact_match_tolerant 0 --expand_n --small_seq_min 60 --skip_reverse_complements --skip_adapters -d custom:${x} --blacklist_genomes $blacklisted_genomes -o probes/m${m}-e${e}-iem30-expandn/${f}.fasta --use_native_dict_when_finding_tolerant_coverage --max_num_processes 2 --verbose > log/m${m}-e${e}-iem30-expandn.${f}.out 2> log/m${m}-e${e}-iem30-expandn.${f}.err"
+                echo "$cmd" >> $COMMANDS
+            done
+        done
+    done
+elif [[ $1 == "parallel" ]]; then
+    source activate /ebs/hybsel-design-runs/tools/envs/hybseldesign-prod
+
+    parallel --jobs 20 --no-notice --progress < $COMMANDS
+
+    source deactivate
+else
+    echo "FATAL: Unknown subcommand"
+    exit 1
+fi
