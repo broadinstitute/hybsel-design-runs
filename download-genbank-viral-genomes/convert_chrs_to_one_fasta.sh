40268212076f3b2fb88de1ba3ca0708a3ec6d235
Hayden Metsky <hmetsky@gmail.com>
2017-10-24 20:53:31 -0400

    Add script to combine segmented genomes across multiple files into one file

---
+++ download-genbank-viral-genomes/convert_chrs_to_one_fasta.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Convert genomes spread across many fastas into one fasta.
+#
+# For segmented genomes, the script download_dataset_fastas.py groups the
+# sequences (one per segment) into genomes and creates one fasta file per
+# genome that has all of the segments in it. In general, this is OK, but
+# for viruses with a lot of sequence data (e.g., influenza_a) it can create
+# an unwieldy number of sequences. This combines all of those fasta files
+# into one, using the sequence header to denote an ID for the genome.
+#
+# Args:
+#  1: directory that contains fastas to combine
+#  2: output fasta
+
+echo -n "" > $2
+
+for f in $(ls -1 $1/*.fasta); do
+    g=$(echo "${f##*/}" | sed 's/\.fasta//')
+    while read line; do
+        if [[ "$line" == ">"* ]]; then
+            echo "$line [genome $g]" >> $2
+        else
+            echo $line >> $2
+        fi
+    done < $f
+done
+
