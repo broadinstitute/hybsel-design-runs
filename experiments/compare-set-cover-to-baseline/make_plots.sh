052c898eb681b4e4eaf8a240032fb55e3fd6fc83
Hayden Metsky <hmetsky@gmail.com>
2017-12-13 13:25:18 -0500

    Add data and plots comparing number of probes for zika
    This uses the sequence data and code from hybsel_design as of
    commit 323b639.

--- experiments/compare-set-cover-to-baseline/make_plots.sh
+++ experiments/compare-set-cover-to-baseline/make_plots.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for dataset in ebola_zaire_with_2014 hepatitis_c hiv1_without_ltr; do
+for dataset in ebola_zaire_with_2014 hepatitis_c hiv1_without_ltr zika; do
     for scale in "linear" "log"; do
         Rscript make_plot.R data/${dataset}/num-probes.tsv plots/${dataset}.${scale}.pdf $dataset $scale
     done
